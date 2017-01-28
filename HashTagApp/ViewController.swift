2f914a0d6c9074351694adcd1923d418c6c4cbc2
Keith Caffrey <keithcaffrey@gmail.com>
2017-01-28 15:25:44 +0000

    First commit

---
+++ HashTagApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HashTagApp
+//
+//  Created by Keith Caffrey on 28/01/2017.
+//  Copyright © 2017 Keith Caffrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-HashTagApp/Pods-HashTagApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HashTagApp : NSObject
+@end
+@implementation PodsDummy_Pods_HashTagApp
+@end
